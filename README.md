a2b980503cc4311bfacd0c131d4ceb2c62749a85
Thomas Sp\u00e4th <thomas.spaeth@kanzlei-spaeth.de>
2022-01-26 21:55:34 +0100

    initial Version

---
+++ README.md
@@ -0,0 +1,56 @@
+#on-boarding.tax-bot
+This ist an interview with docassemble for on-boarding employees in client-companies. 
+
+# TODOs:
+## Automatisierung
+- Warum kein Autofill mehr bei Straße und CO?
+
+Validierung
+- Sozialverslicherungsnummer
+- Einstieg<Ausstiegsdatum
+- IBAN
+
+Felder
+- Datumfeld Einstieg
+- Jahr als erste Wahl
+- Tätigkeiten automatisch lernen und zur Bearbeitung in Profilseite
+- Sachbearbeiter in Profilseite
+- Exportmuster in Textfeld vorgeben
+- Krankenversicherung Vorschlagsliste
+
+Hilfe
+- Hilfe unterschiedlich für Mandant und Mitarbeiter des Mandanten
+- Videos Einfahrung und erläuterung
+- Fachbegriffe erläutern
+- SPAM-Filter Hinweis für Mandanten
+
+Dokumente
+- Mitteilungen in E-Mail an prominenter Stelle
+- Mehrsprachige E-Mail eingestellte Spache + englisch
+- Mehrsprachiges (romämisch und englische) Beschreibung zum drucken
+- Notwendigkeit prüfen:
+  - SV-Ausweis: True
+  - Schwerbehindertenausweis: True
+  - VWL-Vertrag: False
+  - Arbeitsvertrag
+  - ? Bescheinigung über LSt-Abzug/ Anzahl der Beschäftigungstage bei Vorarbeitgebern
+  - Schul-/Studienbescheinigung
+  - del Antrag zur Befreiung von der RV-Pflicht
+  - del Bescheinigung der privaten Krankenversicherung
+  - del Unterlagen Sozialkasse Bau/Maler
+
+Dokumentation
+  - Einzelnen Felder Erläutern
+
+Ablauf
+  - Pflichtfelder einfordern von Unternhemen
+  - Kontrolle von Untemrnehmen einführen
+  - Arbeitsvertrag automatisieren, durch erste Eingaben, das Template je Mandant
+  - Beschäftigungen Zeit bei kurzfristiger Beschäftigung
+
+User
+  - Untemrnehmensprofil an Nummer und PW koppeln
+  - später IDM 
+  - (Mandanten) Benutzer über IDM anlegen
+
+
---
+++ setup.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.onboarding',
+      version='0.0.1',
+      description=('An Interview with docassemble for on-boarding employees'),
+      long_description='#on-boarding.tax-bot\r\nThis ist an interview with docassemble for on-boarding employees in client-companies. \r\n\r\n# TODOs:\r\n## Automatisierung\r\n- Warum kein Autofill mehr bei Straße und CO?\r\n\r\nValidierung\r\n- Sozialverslicherungsnummer\r\n- Einstieg<Ausstiegsdatum\r\n- IBAN\r\n\r\nFelder\r\n- Datumfeld Einstieg\r\n- Jahr als erste Wahl\r\n- Tätigkeiten automatisch lernen und zur Bearbeitung in Profilseite\r\n- Sachbearbeiter in Profilseite\r\n- Exportmuster in Textfeld vorgeben\r\n- Krankenversicherung Vorschlagsliste\r\n\r\nHilfe\r\n- Hilfe unterschiedlich für Mandant und Mitarbeiter des Mandanten\r\n- Videos Einfahrung und erläuterung\r\n- Fachbegriffe erläutern\r\n- SPAM-Filter Hinweis für Mandanten\r\n\r\nDokumente\r\n- Mitteilungen in E-Mail an prominenter Stelle\r\n- Mehrsprachige E-Mail eingestellte Spache + englisch\r\n- Mehrsprachiges (romämisch und englische) Beschreibung zum drucken\r\n- Notwendigkeit prüfen:\r\n  - SV-Ausweis: True\r\n  - Schwerbehindertenausweis: True\r\n  - VWL-Vertrag: False\r\n  - Arbeitsvertrag\r\n  - ? Bescheinigung über LSt-Abzug/ Anzahl der Beschäftigungstage bei Vorarbeitgebern\r\n  - Schul-/Studienbescheinigung\r\n  - del Antrag zur Befreiung von der RV-Pflicht\r\n  - del Bescheinigung der privaten Krankenversicherung\r\n  - del Unterlagen Sozialkasse Bau/Maler\r\n\r\nDokumentation\r\n  - Einzelnen Felder Erläutern\r\n\r\nAblauf\r\n  - Pflichtfelder einfordern von Unternhemen\r\n  - Kontrolle von Untemrnehmen einführen\r\n  - Arbeitsvertrag automatisieren, durch erste Eingaben, das Template je Mandant\r\n  - Beschäftigungen Zeit bei kurzfristiger Beschäftigung\r\n\r\nUser\r\n  - Untemrnehmensprofil an Nummer und PW koppeln\r\n  - später IDM \r\n  - (Mandanten) Benutzer über IDM anlegen\r\n\r\n\r\n',
+      long_description_content_type='text/markdown',
+      author='Dr. Thomas Späth',
+      author_email='thomas.spaeth@kanzlei-spaeth.de',
+      license='The MIT License (MIT)',
+      url='https://kanzlei-spaeth.de',
+      packages=find_packages(),
+      namespace_packages=['docassemble'],
+      install_requires=['qrcode>=6.1'],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/onboarding/', package='docassemble.onboarding'),
+     )
+
